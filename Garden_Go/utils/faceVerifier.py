9e3ec5ec592a7bf095bd67a46fee04ee3456b3fc
Abhishek <abhishekchaudhary0220@gmail.com>
2021-06-19 09:33:04 +0530

    modified little and added requirements

--- Garden_Go/utils/faceVerifier.py
+++ Garden_Go/utils/faceVerifier.py
@@ -1,50 +1,38 @@
-import asyncio
-import io
-import glob
 import os
-import sys
-import time
-import uuid
-import requests
-from urllib.parse import urlparse
-from io import BytesIO
-# To install this module, run:
-# python -m pip install Pillow
-from PIL import Image, ImageDraw
 from azure.cognitiveservices.vision.face import FaceClient
 from msrest.authentication import CognitiveServicesCredentials
-from azure.cognitiveservices.vision.face.models import TrainingStatusType, Person
-from dotenv import load_dotenv
-load_dotenv()
+
+# from dotenv import load_dotenv
+# load_dotenv('../../.env')
 
 
-class face_client():
-    def __init__(self):
+class FaceClientCustom:
+    def __init__(self) -> None:
         self.KEY = os.environ.get('FACEKEY')
         self.ENDPOINT = os.environ.get('FACEENDPOINT')
     
-    def setup(self):
+    def setup(self) -> FaceClient:
         face_client = FaceClient(self.ENDPOINT, CognitiveServicesCredentials(self.KEY))
         return face_client
 
-class faceVerifier():
-    def __init__(self, img1Path: str, img2Url: str):
-        self.faceClient = face_client().setup()
-        self.img1Path = img1Path
-        self.img2Url = img2Url
+
+class FaceVerifier:
+    def __init__(self, img1path: str, img2url: str) -> None:
+        self.faceClient = FaceClientCustom().setup()
+        self.img1Path = img1path
+        self.img2Url = img2url
     
-    def getFaceID(self):
+    def get_face_id(self):
         image = open(self.img1Path, 'r+b')
         faces = self.faceClient.face.detect_with_stream(image, detection_model='detection_03')
         return faces[0].face_id
     
-    def isSame(self):
+    def is_face_same(self):
         multi_image_name = os.path.basename(self.img2Url)
         detected_faces2 = self.faceClient.face.detect_with_url(url=self.img2Url, detection_model='detection_03')
-        
-        second_image_face_IDs = list(map(lambda x: x.face_id, detected_faces2))
-        first_image_face_ID = self.getFaceID()
-        similar_faces = self.faceClient.face.find_similar(face_id=first_image_face_ID, face_ids=second_image_face_IDs)
+        second_image_face_id = list(map(lambda x: x.face_id, detected_faces2))
+        first_image_face_id = self.get_face_id()
+        similar_faces = self.faceClient.face.find_similar(face_id=first_image_face_id, face_ids=second_image_face_id)
         if not similar_faces:
             print('No similar faces found in', multi_image_name, '.')
             return False
@@ -52,24 +40,19 @@ class faceVerifier():
         else:
             print('Similar faces found in', multi_image_name + ':')
             for face in similar_faces:
-                first_image_face_ID = face.face_id
+                first_image_face_id = face.face_id
         
-            face_info = next(x for x in detected_faces2 if x.face_id == first_image_face_ID)
+            face_info = next(x for x in detected_faces2 if x.face_id == first_image_face_id)
             if face_info:
-                print('  Face ID: ', first_image_face_ID)
+                print('  Face ID: ', first_image_face_id)
             return True
-    
 
 
+def test_code():
+    test_img1 = "testImgs/messi.jpeg"
+    test_img2 = "testImgs/cristiano.jpg"
 
-    
-    
-
-if __name__ == "__main__":
-    testImg1 = "testImgs/messi.jpeg"
-    testImg2 = "testImgs/cristiano.jpg"
-
-    testUrl = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Lionel_Messi_20180626_%28cropped%29.jpg"
-    fv = faceVerifier(testImg2, testUrl)
-    print(fv.isSame())
+    test_url = "https://upload.wikimedia.org/wikipedia/commons/d/d9/Lionel_Messi_20180626_%28cropped%29.jpg"
+    fv = FaceVerifier(test_img1, test_url)
+    print(fv.is_face_same())
     print("Done")
